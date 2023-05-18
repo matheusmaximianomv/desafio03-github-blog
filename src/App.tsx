1192eceba2c7ab6a6738d0a2ab81015ab2a70b47
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2023-05-17 22:24:22 -0300

    style: Definindo tema padrão, reset de estilização e sobscrição de tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
-  return <h1>Github Blog</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Github Blog</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
