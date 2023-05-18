67dca392856148a77c0252527d69dcd34123560b
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2023-05-18 00:07:38 -0300

    feat: Layout para fixação do header

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const DefaultLayoutContainer = styled.main`
+  max-width: 54rem;
+  margin: 4rem auto 0;
+
+  display: flex;
+  flex-direction: column;
+`;
